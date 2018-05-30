db2b4faf22b600156459dd6d939e4cf1a53feccf
Catherine Bronzo <cbronzo@mac.com>
2018-05-30 03:40:01 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,12 +3,12 @@
 
 board = ["X"," ","O"," "," "," ","X"," ","O",]
 def display_board(board)
-  puts "   |   |   "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-display_board
+display_board(board)
 
-#{board[0]}
+ 
